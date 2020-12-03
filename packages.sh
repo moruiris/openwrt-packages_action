4160de7c1bc40beb9e4a0616d75f8258949d686d
morui <53965900+moruiris@users.noreply.github.com>
2020-12-03 18:22:11 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # 自用软件
-svn co https://github.com/kenzok8/openwrt-packages/trunk/AdGuardHome
+git clone https://github.com/kenzok8/openwrt-packages/trunk/AdGuardHome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
-svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns
+git clone https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
+git clone https://github.com/kenzok8/openwrt-packages/trunk/smartdns
 git clone https://github.com/cnzd/luci-app-koolproxyR
 git clone https://github.com/xiaorouji/openwrt-passwall
 git clone https://github.com/fw876/helloworld
-svn co https://github.com/jerrykuku/luci-app-vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git
+git clone https://github.com/jerrykuku/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb
 # 自用主题
-svn co https://github.com/kenzok8/luci-theme-ifit/trunk/luci-theme-ifit
-svn co https://github.com/jerrykuku/luci-theme-argon
+git clone https://github.com/kenzok8/luci-theme-ifit/trunk/luci-theme-ifit
+git clone https://github.com/jerrykuku/luci-theme-argon
 
 
 rm -rf ./*/.git
