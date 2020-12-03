2d49c493572018afede1859752a22f4e42501c27
morui <53965900+moruiris@users.noreply.github.com>
2020-12-03 18:27:53 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # 自用软件
-git clone https://github.com/kenzok8/openwrt-packages/trunk/AdGuardHome
+svn co https://github.com/kenzok8/openwrt-packages/trunk/AdGuardHome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome
-git clone https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
-git clone https://github.com/kenzok8/openwrt-packages/trunk/smartdns
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
+svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns
 git clone https://github.com/cnzd/luci-app-koolproxyR
 git clone https://github.com/xiaorouji/openwrt-passwall
 git clone https://github.com/fw876/helloworld
 git clone https://github.com/jerrykuku/luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb
 # 自用主题
-git clone https://github.com/kenzok8/luci-theme-ifit/trunk/luci-theme-ifit
+git clone https://github.com/kenzok8/luci-theme-ifit
 git clone https://github.com/jerrykuku/luci-theme-argon
 
 
