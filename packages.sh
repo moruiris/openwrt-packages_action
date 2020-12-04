49f6ee7bd41abb7abeff007e5a6e3e66ee2e1bd7
morui <53965900+moruiris@users.noreply.github.com>
2020-12-04 15:20:28 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -12,7 +12,6 @@ git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/jerrykuku/luci-app-vssr.git
 # 自用主题
 git clone https://github.com/kenzok8/luci-theme-ifit
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 
 
 rm -rf ./*/.git
