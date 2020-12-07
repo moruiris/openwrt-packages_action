81a46b9a1a5e56a7b7fddd71b12254aa63b14ce1
morui <53965900+moruiris@users.noreply.github.com>
2020-12-07 22:09:29 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -5,7 +5,7 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome
 svn co https://github.com/kenzok8/openwrt-packages/trunk/AdGuardHome
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
 svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns
-git clone https://github.com/cnzd/luci-app-koolproxyR
+# 弃用git clone https://github.com/cnzd/luci-app-koolproxyR
 git clone https://github.com/xiaorouji/openwrt-passwall
 git clone https://github.com/fw876/helloworld
 git clone https://github.com/jerrykuku/lua-maxminddb.git
