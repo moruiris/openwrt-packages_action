de764edb4466c07ee38d6cb0e4d13d095ecfb268
morui <53965900+moruiris@users.noreply.github.com>
2021-01-09 21:00:46 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # 自用软件
+git clone https://github.com/vernesong/OpenClash.git
+mv -f OpenClash/luci-app-openclash .
+rm -rf OpenClash
 git clone https://github.com/rufengsuixing/luci-app-adguardhome
 svn co https://github.com/kenzok8/openwrt-packages/trunk/AdGuardHome
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
@@ -10,6 +13,7 @@ git clone https://github.com/xiaorouji/openwrt-passwall
 git clone https://github.com/fw876/helloworld
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/jerrykuku/luci-app-vssr.git
+
 # 自用主题
 git clone https://github.com/kenzok8/luci-theme-ifit
 
