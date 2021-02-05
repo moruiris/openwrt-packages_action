c8917772883e0a2c068f5cb4c069ddb0724ef9b4
morui <53965900+moruiris@users.noreply.github.com>
2021-02-06 00:20:10 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # 自用软件
+
 git clone https://github.com/vernesong/OpenClash.git
 mv -f OpenClash/luci-app-openclash .
 rm -rf OpenClash
@@ -13,10 +14,12 @@ svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
 svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns
 # 弃用git clone https://github.com/cnzd/luci-app-koolproxyR
 git clone https://github.com/xiaorouji/openwrt-passwall
-git clone https://github.com/fw876/helloworld
+# 原作者
+# git clone https://github.com/fw876/helloworld
+# 二次更改
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/jerrykuku/luci-app-vssr.git
-
+git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git
 # 自用主题
 git clone https://github.com/kenzok8/luci-theme-ifit
 
