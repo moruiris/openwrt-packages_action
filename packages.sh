86d38eddffd78fc7110f318160bc075dff8fc253
morui <53965900+moruiris@users.noreply.github.com>
2021-04-19 19:56:59 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# 弃用
+# git clone https://github.com/jefferymvp/luci-app-koolproxyR
+# git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git
+
+# 原作者
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome
+# git clone https://github.com/fw876/helloworld
 
 # 自用软件
 # luci-app-netdata----------------------网络监控中文版
@@ -6,27 +13,21 @@ git clone https://github.com/sirpdboy/luci-app-netdata
 git clone https://github.com/vernesong/OpenClash.git
 mv -f OpenClash/luci-app-openclash .
 rm -rf OpenClash
-# 原作者
-# git clone https://github.com/rufengsuixing/luci-app-adguardhome
-# 二次更改
 git clone https://github.com/kongfl888/luci-app-adguardhome
 svn co https://github.com/kenzok8/openwrt-packages/trunk/AdGuardHome
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
 svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns
-# 弃用git clone https://github.com/jefferymvp/luci-app-koolproxyR
 git clone https://github.com/xiaorouji/openwrt-passwall
-# 原作者
-# git clone https://github.com/fw876/helloworld
-# 二次更改
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/jerrykuku/luci-app-vssr.git
-# 网易云二次更改
-# git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git
+git clone https://github.com/BCYDTZ/luci-app-UUGameAcc
+
 # 自用主题
 git clone https://github.com/kenzok8/luci-theme-ifit
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config
+svn co https://github.com/kenzok8/litte/trunk/luci-theme-argon_new
 
 rm -rf ./*/.git
 rm -rf ./*/.svn 
